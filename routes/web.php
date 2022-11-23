0b609876745d3abc596b02fce5617e2f80c6ba52
An Thai <thaitranminhan@gmail.com>
2022-11-23 09:41:53 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\HomeController;
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [IndexController::class, 'home'])->name('homepage');
+Route::get('/category', [IndexController::class, 'category'])->name('category');
+Route::get('/country', [IndexController::class, 'country'])->name('country');
+Route::get('/watch', [IndexController::class, 'watch'])->name('watch');
+Route::get('/movie', [IndexController::class, 'movie'])->name('movie');
+
+Auth::routes();
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
